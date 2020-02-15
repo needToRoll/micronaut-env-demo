f234c8003e9f279cb44baca4889389df74b716bd
Florian Bürgi <fb.buergi@gmx.ch>
2020-02-15 18:35:49 +0100

    initial commit

---
+++ src/main/java/micronaut/env/demo/PropertyReader.java
@@ -0,0 +1,18 @@
+package micronaut.env.demo;
+
+import io.micronaut.context.annotation.Value;
+import io.micronaut.context.event.StartupEvent;
+import io.micronaut.runtime.event.annotation.EventListener;
+import javax.inject.Singleton;
+
+@Singleton
+public class PropertyReader {
+    @Value("${envdemo.greeting}")
+    private String propertyFromOsEnv;
+
+    @EventListener
+    public void greetOnStartUp(StartupEvent event){
+        String message = String.format("Micronaut says: %s from %s", propertyFromOsEnv, System.getProperty("os.name"));
+        System.out.println(message);
+    }
+}
